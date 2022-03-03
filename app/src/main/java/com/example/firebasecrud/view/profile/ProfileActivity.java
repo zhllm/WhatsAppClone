3a81ca6dcaa2bcd416104c33f7b05722590b4239
zhllm <beyourselfinsist@gmail.com>
2022-03-03 10:42:34 +0800

    profile upload image to firebase

--- app/src/main/java/com/example/firebasecrud/view/profile/ProfileActivity.java
+++ app/src/main/java/com/example/firebasecrud/view/profile/ProfileActivity.java
@@ -1,16 +1,237 @@
 package com.example.firebasecrud.view.profile;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
 
+import android.content.ContentResolver;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.util.Log;
+import android.view.View;
+import android.view.WindowManager;
+import android.webkit.MimeTypeMap;
+import android.widget.Toast;
 
+import com.bumptech.glide.Glide;
 import com.example.firebasecrud.R;
+import com.example.firebasecrud.databinding.ActivityProfileBinding;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.material.bottomsheet.BottomSheetDialog;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+
+import java.util.Objects;
 
 public class ProfileActivity extends AppCompatActivity {
+    ActivityProfileBinding binding;
+    private FirebaseUser firebaseUser;
+    private FirebaseFirestore fireStore;
+    private BottomSheetDialog dialog;
+    private int IMAGE_GALLERY_REQUEST = 111;
+    final static String TAG = "CommonLogTag";
+    private Uri imageUri;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_profile);
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_profile);
+        setSupportActionBar(binding.toolbar);
+        Objects.requireNonNull(getSupportActionBar()).setDisplayShowHomeEnabled(true);
+        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+        if (user != null) {
+            FirebaseFirestore.getInstance().collection("Users").document(user.getUid()).get().addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
+                @Override
+                public void onSuccess(DocumentSnapshot documentSnapshot) {
+                    String imageProfile = Objects.requireNonNull(documentSnapshot.get("userProfile")).toString();
+                    if (!imageProfile.isEmpty()) {
+                        binding.imageProfile.setImageURI(Uri.parse(imageProfile));
+                    }
+                }
+            }).addOnFailureListener(new OnFailureListener() {
+                @Override
+                public void onFailure(@NonNull Exception e) {
+
+                }
+            });
+        }
+        fireStore = FirebaseFirestore.getInstance();
+        firebaseUser = FirebaseAuth.getInstance().getCurrentUser();
+
+        if (firebaseUser != null) {
+            getInfo();
+        }
+
+        initActionClick();
+    }
+
+    private void initActionClick() {
+        binding.fabCamera.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showBottomSheetPickPhoto();
+            }
+        });
+    }
+
+    private void showBottomSheetPickPhoto() {
+        dialog = new BottomSheetDialog(this);
+        View view = getLayoutInflater().inflate(R.layout.bottom_sheet_pick, null);
+        view.findViewById(R.id.ln_gallery).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openGallery();
+                dialog.dismiss();
+            }
+        });
+
+        view.findViewById(R.id.ln_camera).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(), "Camera", Toast.LENGTH_SHORT).show();
+//                dispatchTakePictureIntent();
+                dialog.dismiss();
+            }
+        });
+
+
+        dialog.setContentView(view);
+        dialog.getWindow().addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+        dialog.setOnDismissListener(new DialogInterface.OnDismissListener() {
+            @Override
+            public void onDismiss(DialogInterface dialog) {
+                Log.d(TAG, "dialog dismiss");
+                dialog = null;
+            }
+        });
+        dialog.show();
+    }
+
+    private void openGallery() {
+        Intent intent = new Intent();
+        intent.setType("image/*");
+        intent.setAction(Intent.ACTION_GET_CONTENT);
+        startActivityForResult(Intent.createChooser(intent, "select image"), IMAGE_GALLERY_REQUEST);
+
+//        ActivityResultLauncher<Intent> activityResultLauncher = registerForActivityResult(
+//                new ActivityResultContracts.StartActivityForResult(),
+//                new ActivityResultCallback<ActivityResult>() {
+//                    @Override
+//                    public void onActivityResult(ActivityResult result) {
+//
+//                    }
+//                }
+//        );
+//
+//        Intent intent = new Intent();
+//        intent.setType("image/*");
+//        intent.setAction(Intent.ACTION_GET_CONTENT);
+//        activityResultLauncher.launch(Intent.createChooser(intent, "select image"));
+    }
+
+    private void getInfo() {
+        fireStore.collection("Users")
+                .document(firebaseUser.getUid())
+                .get()
+                .addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
+                    @Override
+                    public void onSuccess(DocumentSnapshot documentSnapshot) {
+                        Log.d(TAG, "getInfo:onSuccess:");
+                        String userName = documentSnapshot.get("userName").toString();
+                        String userPhone = documentSnapshot.get("userPhone").toString();
+                        String userProfile = documentSnapshot.get("userProfile").toString();
+                        binding.tvUsername.setText(userName);
+                        binding.tvPhone.setText(userPhone);
+                        if (!userProfile.isEmpty()) {
+                            Glide.with(ProfileActivity.this)
+                                    .load(userProfile)
+                                    .into(binding.imageProfile);
+                        }
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Log.d(TAG, "getInfo:OnFailureListener:Exception:" + e.getMessage());
+                    }
+                });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == IMAGE_GALLERY_REQUEST
+                && resultCode == RESULT_OK
+                && data != null
+                && data.getData() != null) {
+            imageUri = data.getData();
+            uploadToFirebase();
+            try {
+                Bitmap bitmap = MediaStore.Images.Media.getBitmap(getContentResolver(), imageUri);
+                binding.imageProfile.setImageBitmap(bitmap);
+            } catch (Exception e) {
+                e.printStackTrace();
+                Log.d(TAG, "onActivityResult:" + e.getMessage());
+            }
+        }
+    }
+
+    private void uploadToFirebase() {
+        if (imageUri != null) {
+            StorageReference reference = FirebaseStorage.getInstance().getReference()
+                    .child("ImagesProfiles/" + System.currentTimeMillis() + "." +
+                            getFileExtension(imageUri));
+            reference.putFile(imageUri)
+                    .addOnFailureListener(new OnFailureListener() {
+                        @Override
+                        public void onFailure(@NonNull Exception e) {
+                            Log.d(TAG, "uploadToFirebase:Exception" + e.getMessage());
+                        }
+                    })
+                    .addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                        @Override
+                        public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                            Task<Uri>  uriTask = taskSnapshot.getStorage().getDownloadUrl();
+                            while (!uriTask.isSuccessful());
+                            Uri downloadUri = uriTask.getResult();
+                            final String sDownloadUrl = String.valueOf(downloadUri);
+
+                            FirebaseFirestore.getInstance().collection("Users")
+                                    .document(FirebaseAuth.getInstance().getCurrentUser().getUid())
+                                    .update("userProfile", sDownloadUrl)
+                                    .addOnSuccessListener(new OnSuccessListener<Void>() {
+                                        @Override
+                                        public void onSuccess(Void unused) {
+                                            Log.d(TAG, "update user profile :success");
+                                        }
+                                    })
+                                    .addOnFailureListener(new OnFailureListener() {
+                                        @Override
+                                        public void onFailure(@NonNull Exception e) {
+                                            Log.d(TAG, "update user profile :error: " + e.getMessage());
+                                        }
+                                    });
+                        }
+                    });
+        }
+    }
+
+    private String getFileExtension(Uri imageUri) {
+        ContentResolver resolver = getContentResolver();
+        MimeTypeMap mimeTypeMap = MimeTypeMap.getSingleton();
+        return mimeTypeMap.getExtensionFromMimeType(resolver.getType(imageUri));
     }
 }
--- app/src/main/java/com/example/firebasecrud/view/settings/SettingsActivity.java
+++ app/src/main/java/com/example/firebasecrud/view/settings/SettingsActivity.java
@@ -4,11 +4,16 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.databinding.DataBindingUtil;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 
+import com.bumptech.glide.Glide;
 import com.example.firebasecrud.R;
 import com.example.firebasecrud.databinding.ActivitySettingsBinding;
+import com.example.firebasecrud.view.profile.ProfileActivity;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.auth.FirebaseAuth;
@@ -34,6 +39,16 @@ public class SettingsActivity extends AppCompatActivity {
         if (firebaseUser != null) {
             getInfo();
         }
+        initClickAction();
+    }
+
+    private void initClickAction() {
+        binding.lnProfile.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(SettingsActivity.this, ProfileActivity.class));
+            }
+        });
     }
 
     private void getInfo() {
@@ -44,6 +59,10 @@ public class SettingsActivity extends AppCompatActivity {
                     @Override
                     public void onSuccess(DocumentSnapshot documentSnapshot) {
                         String userName = Objects.requireNonNull(documentSnapshot.get("userName")).toString();
+                        String userProfile = documentSnapshot.getString("userProfile");
+                        Glide.with(SettingsActivity.this)
+                                .load(Uri.parse(userProfile))
+                                .into(binding.userProfile);
                         binding.userName.setText(userName);
                         Log.d(TAG, TAG + " success: userName = " + userName);
                     }
